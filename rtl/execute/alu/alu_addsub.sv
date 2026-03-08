c411f92f8ede4e9448bbdf4f9df4ecb11dae168c
Marco Benkler <marcobenkler@users.noreply.github.com>
2026-03-08 17:08:52 +0100

    Expand folder-structure, add alu_add/sub module

---
+++ rtl/execute/alu/alu_addsub.sv
@@ -0,0 +1,19 @@
+/*
+* @brief ALU add/sub unit
+*
+* Performs integer addition/subtraction.
+*/
+
+module alu_addsub (
+    input   logic [31:0] a,     ///< first operand
+    input   logic [31:0] b,     ///< second operand
+    input   logic sub,          ///< 1 = sub, 0 = add
+    output  logic [31:0] result ///< result
+);
+
+    logic [31:0] b_buf;
+    
+    assign b_buf = sub ? ~b : b;
+    assign result = a + b_buf + sub;
+
+endmodule
