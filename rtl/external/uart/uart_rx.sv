cb00f8d4d73a12451b4b2ec59bb4accabe1ade17
Marco Benkler <marcobenkler@users.noreply.github.com>
2026-05-24 15:35:08 +0200

    fix amigious clock

--- rtl/external/uart/uart_rx.sv
+++ rtl/external/uart/uart_rx.sv
@@ -19,12 +19,10 @@ module uart_rx
     always_ff @(posedge clk or negedge reset_n) begin
         if (!reset_n) begin
             state      <= IDLE;
-            tick_count <= '0;
             wdata      <= '0;
         end
         else if (state == IDLE && rx == 1'b0) begin
             state     <= START;
-            tick_count <= '0;
         end
         else if (state == START && baud_tick) begin
             state   <= DATA;
@@ -38,8 +36,13 @@ module uart_rx
         else if (state == STOP && baud_tick) begin
             state <= IDLE;
         end
-        if (baud_tick_fast) begin
-            if(tick_count == 4'b1111) tick_count <= '0;
+    end
+
+    always_ff @(posedge clk or negedge reset_n) begin
+        if (!reset_n) tick_count <= '0;
+        else if (state == IDLE && rx == 1'b0) tick_count <= '0;  // resync
+        else if (baud_tick_fast) begin
+            if (tick_count == 4'b1111) tick_count <= '0;
             else tick_count <= tick_count + 1;
         end
     end
